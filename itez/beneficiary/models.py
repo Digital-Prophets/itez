68643ad094bb2677631c0579572e84510f609318
Jachin Manda <jachin.dev@outlook.com>
2021-11-04 13:38:57 +0200

    Reformat code

--- itez/beneficiary/models.py
+++ itez/beneficiary/models.py
@@ -11,6 +11,12 @@ class Beneficiary(models.Model):
     Implements the Beneficiary properties and required methods.
     """
 
+    GENDER = (
+        ("male", _("Male")),
+        ("female", _("Female")),
+        ("other", _("Other"))
+    )
+  
     MARITAL_STATUS = (
         ("single", _("Single")),
         ("married", _("Married")),
@@ -19,19 +25,36 @@ class Beneficiary(models.Model):
         ("widowed", _("Widowed")),
     )
 
-    GENDER = (("male", _("Male")), ("female", _("Female")), ("other", _("Other")))
-
-    first_name = models.CharField(_("First Name"), max_length=200)
-    last_name = models.CharField(_("Last Name"), max_length=200)
-    other_name = models.CharField(_("Other Name"), max_length=200, null=True)
-    gender = models.CharField(_("Gender"), max_length=100, choices=GENDER)
-    profile_photo = models.ImageField(upload_to="profile_photo/")
+    first_name = models.CharField(
+        _("First Name"),
+        max_length=200
+    )
+    last_name = models.CharField(
+        _("Last Name"),
+        max_length=200
+    )
+    other_name = models.CharField(
+        _("Other Name"),
+        max_length=200,
+        null=True
+    )
+    gender = models.CharField(
+        _("Gender"),
+        max_length=100,
+        choices=GENDER
+    )
+    profile_photo = models.ImageField(
+        upload_to="profile_photo/"
+    )
     beneficiary_code = models.UUIDField(
-        default=generate_uuid_and_agent_code()[0], editable=False
+        default=generate_uuid_and_agent_code()[0],
+        editable=False
     )
     date_of_birth = models.DateField(_("Date of Birth"))
     marital_status = models.CharField(
-        _("Marital Status"), choices=MARITAL_STATUS, max_length=100
+        _("Marital Status"),
+        choices=MARITAL_STATUS,
+        max_length=100
     )
     created = models.DateTimeField(auto_now_add=True)
 
@@ -101,11 +124,25 @@ class WorkDetail(models.Model):
     """
 
     gross_pay = models.DecimalField(
-        _("Monthly Salary"), decimal_places=2, max_digits=1000, null=False
+        _("Monthly Salary"),
+        decimal_places=2,
+        max_digits=1000,
+        null=False
     )
-    company = models.CharField(_("Company Name"), max_length=200, null=False)
-    insured = models.BooleanField(_("Company Insured"), default=False)
-    work_address = models.TextField(_("Work Address"), null=True, blank=True)
+    company = models.CharField(
+        _("Company Name"),
+        max_length=200,
+        null=False
+    )
+    insured = models.BooleanField(
+        _("Company Insured"),
+        default=False
+    )
+    work_address = models.TextField(
+        _("Work Address"),
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return self.company
