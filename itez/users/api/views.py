f2f2fc6e198928578db58797494c7dc1453d1d2c
Jachin Manda <jachin.dev@outlook.com>
2021-11-12 13:25:31 +0200

    Align imports top to bottom

--- itez/users/api/views.py
+++ itez/users/api/views.py
@@ -4,11 +4,21 @@ from django.contrib.auth.models import Group
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, CreateModelMixin, UpdateModelMixin, DestroyModelMixin
+from rest_framework.mixins import (
+    ListModelMixin,
+    RetrieveModelMixin,
+    CreateModelMixin,
+    UpdateModelMixin,
+    DestroyModelMixin
+)
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
-from .serializers import UserSerializer, ChangePasswordSerializer, GroupModelSerializer
+from .serializers import (
+    UserSerializer,
+    ChangePasswordSerializer,
+    GroupModelSerializer
+)
 
 User = get_user_model()
 
