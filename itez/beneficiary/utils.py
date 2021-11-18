4cfec6df2dfe2ccc28c1921b0e9edc92d5026a81
Jachin Manda <jachin.dev@outlook.com>
2021-11-18 10:42:17 +0200

    Refactor util function

--- itez/beneficiary/utils.py
+++ itez/beneficiary/utils.py
@@ -5,7 +5,8 @@ def generate_uuid_and_agent_code():
     """
     Generates a seven digit agent ID and a unique ID.
     """
-    uu_id = uuid.uuid4()
-    agent_code = str(uu_id)[1:8].upper()
+    unique_id = uuid.uuid4()
+    agent_code = f"Agent-{str(unique_id)[1:8].upper()}"
+    beneficiary_code = f"Beneficiary-{str(unique_id)[1:8].upper()}"
     
-    return uu_id, agent_code
+    return beneficiary_code, agent_code
