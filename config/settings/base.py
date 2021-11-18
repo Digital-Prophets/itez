118bfc7ab087859a96b8f1315938b93f0d8cbb64
Jachin Manda <jachin.dev@outlook.com>
2021-11-18 15:00:21 +0200

    Update Rest Framework configs

--- config/settings/base.py
+++ config/settings/base.py
@@ -318,6 +318,9 @@ STATICFILES_FINDERS += ["compressor.finders.CompressorFinder"]
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication'
+
     ),
     "DEFAULT_PERMISSION_CLASSES": [], #("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
