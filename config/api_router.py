11bcca089fd0799cda2d5304a357448c4020ef4b
Alison Mukoma <alison.mukoma@trivago.com>
2021-11-25 16:42:53 +0100

    realign enspoints routes

--- config/api_router.py
+++ config/api_router.py
@@ -32,25 +32,91 @@ else:
     router = SimpleRouter()
 
 
-router.register("user", UserViewSet)
-router.register("users/change_password", ChangePasswordView)
-router.register("role", RoleAPIView, basename="role")
-router.register("drug", DrugAPIView, basename='drug')
-router.register("labs", LabAPIView, basename='lab')
-router.register("prescription", PrescriptionAPIView, basename='prescriptions')
-router.register("facility_type", FacilityTypeAPIView, basename='facicility_type')
-router.register("facility", FacilityAPIView, basename='facicility')
-router.register("implementing_partner", ImplementingPartnerAPIView, basename='implementing_partner')
-router.register("service", ServiceAPIView, basename='service')
-router.register("service_provider_personnel", ServiceProviderPersonelAPIView, basename='service_provider_personnel')
-router.register("service_provider_personnel_qualification", ServiceProviderPersonelQualificationAPIView, basename='service_provider_personnel_qualification')
-router.register("agent", AgentDetailAPIView)
-router.register("beneficiarie", BeneficiaryAPIView)
-router.register("beneficiary_parent", BeneficiaryParentAPIView)
-router.register("province", ProvinceAPIView)
-router.register("district", DistrictAPIView)
-router.register('service_area', ServiceAreaAPIView)
-router.register('work_detail', WorkDetailAPIView)
+router.register(
+    "user", 
+    UserViewSet
+    )
+router.register(
+    "users/change_password", 
+    ChangePasswordView
+    )
+router.register(
+    "role", 
+    RoleAPIView, 
+    basename="role"
+    )
+router.register(
+    "drug", 
+    DrugAPIView, 
+    basename='drug'
+    )
+router.register(
+    "labs", 
+    LabAPIView, 
+    basename='lab'
+    )
+router.register(
+    "prescription", 
+    PrescriptionAPIView, 
+    basename='prescriptions'
+    )
+router.register(
+    "facility_type", 
+    FacilityTypeAPIView, 
+    )
+router.register(
+    "facility", 
+    FacilityAPIView, 
+    basename='facicility'
+    )
+router.register(
+    "implementing_partner", 
+    ImplementingPartnerAPIView, 
+    basename='implementing_partner'
+    )
+router.register(
+    "service", 
+    ServiceAPIView, 
+    basename='service'
+    )
+router.register(
+    "service_provider_personnel", 
+    ServiceProviderPersonelAPIView, 
+    basename='service_provider_personnel'
+    )
+router.register(
+    "service_provider_personnel_qualification", 
+    ServiceProviderPersonelQualificationAPIView, 
+    basename='service_provider_personnel_qualification'
+    )
+router.register(
+    "agent", 
+    AgentDetailAPIView
+    )
+router.register(
+    "beneficiarie", 
+    BeneficiaryAPIView
+    )
+router.register(
+    "beneficiary_parent", 
+    BeneficiaryParentAPIView
+    )
+router.register(
+    "province", 
+    ProvinceAPIView
+    )
+router.register(
+    "district", 
+    DistrictAPIView
+    )
+router.register(
+    "service_area", 
+    ServiceAreaAPIView
+    )
+router.register(
+    "work_detail", 
+    WorkDetailAPIView
+    )
 
 app_name = "api"
 urlpatterns = router.urls
