585ba24dfe97c265655d983220032a7b7c13c3e5
Jachin Manda <jachin.dev@outlook.com>
2021-12-15 10:42:11 +0200

    Add task for generating medical record and update task for downloading beneficiary data

--- itez/beneficiary/tasks.py
+++ itez/beneficiary/tasks.py
@@ -1,18 +1,18 @@
-import time
 import os
-import sys
-import json
-
 from datetime import datetime
+
 from django.conf import settings
+
+from celery import shared_task
 
 from config import celery_app
 
+from .models import Beneficiary, MedicalRecord
 from .resources import BeneficiaryResource
-from celery import shared_task
+from .utils import GenerateMedicalReport
 
 
-@shared_task(bind=True, name="itez")
+@shared_task(bind=True)
 def generate_export_file(self):
     """
     This task generates a file containing all Beneficiary data for export, stores for file temporary, and return
@@ -21,7 +21,7 @@ def generate_export_file(self):
     beneficiary_resource = BeneficiaryResource()
     dataset = beneficiary_resource.export()
     filename = save_exported_data(data=dataset.xlsx, file_ext='.xlsx')
-    return filename
+    return {"TASK_TYPE": "EXPORT_BENEFICIARY_DATA", "RESULT": filename}
 
 
 def save_exported_data(data=None, file_ext=None):
@@ -36,3 +36,24 @@ def save_exported_data(data=None, file_ext=None):
     with open(f"{file_export_path}/{filename}", "wb") as f:
         f.write(data)
         return filename
+
+
+@celery_app.task()
+def generate_medical_report(id):
+    beneficiary_obj = Beneficiary.objects.get(id=id)
+    medical_records = MedicalRecord.objects.filter(beneficiary__id=beneficiary_obj.id)
+
+    timestamp = datetime.now().strftime("%H_%M_%S_%f")
+    directory = f"{settings.MEDIA_ROOT}/beneficiary_report"
+
+    
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+
+    timestamp = datetime.now().strftime("%H_%M_%S_%f")
+    filename = f"{beneficiary_obj.beneficiary_id}_{timestamp}.pdf"
+    full_filepath = os.path.join(directory, filename)
+
+    GenerateMedicalReport(full_filepath, beneficiary_obj, medical_records)
+
+    return {"TASK_TYPE": "GENERATE_MEDICAL_REPORT", "RESULT": filename}
