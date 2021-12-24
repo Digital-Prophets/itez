ab8acb9942b9b7112fe477d72fcb99a76d306a8b
Jachin Manda <jachin.dev@outlook.com>
2021-12-24 21:54:27 +0200

    Align beneficiary info in one column and reformat code.

--- itez/beneficiary/pdf_utils.py
+++ itez/beneficiary/pdf_utils.py
@@ -8,7 +8,9 @@ date_today = datetime.date.today().strftime("%b %d %Y")
 
 
 class PDF(FPDF, HTMLMixin):
-    def __init__(self, *args, beneficiary_obj=None, medical_records=None, **kwargs) -> None:
+    def __init__(
+        self, *args, beneficiary_obj=None, medical_records=None, **kwargs
+    ) -> None:
         self.beneficiary_obj = beneficiary_obj
         self.medical_records = medical_records
         super().__init__(*args, **kwargs)
@@ -24,7 +26,6 @@ class PDF(FPDF, HTMLMixin):
         self.cell(0, 10, f"Page {self.page_no()}", align="C")
         self.print_footer(self.beneficiary_obj.beneficiary_id)
 
-
     def print_header(self, height, title, font_size):
         self.set_font("helvetica", "B", font_size)
 
@@ -33,22 +34,22 @@ class PDF(FPDF, HTMLMixin):
 
     def beneficiary_info_to_dict(self):
         beneficiary_data = {
-            "Name": f"{self.beneficiary_obj.first_name} {self.beneficiary_obj.last_name}",
-            "Gender": self.beneficiary_obj.gender,
-            "Sex": self.beneficiary_obj.sex,
-            "Phone Number": self.beneficiary_obj.phone_number,
-            "Email": self.beneficiary_obj.email,
-            "Date of Birth": self.beneficiary_obj.date_of_birth,
-            "Marital Status": self.beneficiary_obj.marital_status,
-            "Address": self.beneficiary_obj.address,
-            "ART Status": self.beneficiary_obj.art_status,
-            "HIV Status": self.beneficiary_obj.hiv_status,
-            "Registered Facility": self.beneficiary_obj.registered_facility,
-            "Name of Spouse": self.beneficiary_obj.name_of_spouse,
-            "Number of Children": self.beneficiary_obj.number_of_children,
-            "Number of Siblings": self.beneficiary_obj.number_of_siblings,
-            "Education Leval": self.beneficiary_obj.education_level,
-            "Alive": "Yes" if self.beneficiary_obj.alive else "No"
+            "Name:": f"{self.beneficiary_obj.first_name} {self.beneficiary_obj.last_name}",
+            "Gender:": self.beneficiary_obj.gender,
+            "Sex:": self.beneficiary_obj.sex,
+            "Phone Number:": self.beneficiary_obj.phone_number,
+            "Email:": self.beneficiary_obj.email,
+            "Date of Birth:": self.beneficiary_obj.date_of_birth,
+            "Marital Status:": self.beneficiary_obj.marital_status,
+            "Address:": self.beneficiary_obj.address,
+            "ART Status:": self.beneficiary_obj.art_status,
+            "HIV Status:": self.beneficiary_obj.hiv_status,
+            "Registered Facility:": self.beneficiary_obj.registered_facility,
+            "Name of Spouse:": self.beneficiary_obj.name_of_spouse,
+            "Number of Children:": self.beneficiary_obj.number_of_children,
+            "Number of Siblings:": self.beneficiary_obj.number_of_siblings,
+            "Education Leval:": self.beneficiary_obj.education_level,
+            "Alive:": "Yes" if self.beneficiary_obj.alive else "No",
         }
         return beneficiary_data
 
@@ -58,7 +59,11 @@ class PDF(FPDF, HTMLMixin):
         self.image(f"{settings.STATIC_ROOT}/assets/moh_logo.png", 170, 8, 30)
         self.set_font("helvetica", "B", 16)
 
-        self.print_header(35, f"Medical Record For: {self.beneficiary_obj.first_name} {self.beneficiary_obj.last_name}", 18)
+        self.print_header(
+            35,
+            f"Medical Record For: {self.beneficiary_obj.first_name} {self.beneficiary_obj.last_name}",
+            18,
+        )
 
         self.set_font("helvetica", "B", 13)
         self.cell(70, 8, f"Date: {date_today}", ln=1)
@@ -68,32 +73,42 @@ class PDF(FPDF, HTMLMixin):
         self.set_font("helvetica", "", 11)
 
         self.set_y(70)
-        row_count = 1
-        y_axis = 70
+
+        table_data = []
         for field_name, value in self.beneficiary_info_to_dict().items():
             if not value:
                 continue
-            # self.set_y(50)
-            if row_count >= 9:
-                self.set_y(y_axis)
-                self.set_x(80)
-                self.cell(60, 8, f"{field_name}: {value}", ln=1)
-                row_count += 1
-                y_axis += 9.5
-            else:
-                if field_name == "Address":
-                    self.multi_cell(60, 8, f"{field_name}: {value}")
+            table_data.append([field_name, str(value)])
+
+        line_height = self.font_size * 2
+        col_width = self.epw / 4  # distribute content evenly
+        for row in table_data:
+            for index, datum in enumerate(row):
+                if index == 0:
+                    self.multi_cell(
+                        col_width + 5,
+                        line_height,
+                        datum,
+                        ln=3,
+                        max_line_height=self.font_size,
+                    )
                 else:
-                    self.cell(60, 8, f"{field_name}: {value}", ln=1)
-                row_count += 1
+                    self.multi_cell(
+                        col_width + 30,
+                        line_height,
+                        datum,
+                        ln=3,
+                        max_line_height=self.font_size,
+                    )
+            self.ln(line_height)
+
         self.set_y(220)
         self.set_x(45)
         self.set_font("helvetica", "B", 12)
         self.image(f"{self.beneficiary_obj.profile_photo.path}", 155, 85, 50)
 
         record_approver = self.medical_records.first()
-        # print(dir(self.medical_records))
-        # self.cell(60, 8, f"Approved By: {record_approver.approved_by}", ln=1, align='C')
+
         self.set_x(70)
         self.cell(60, 8, f"Approved By: {record_approver.approved_by}", ln=1)
         if record_approver.approver_signature:
@@ -107,7 +122,6 @@ class PDF(FPDF, HTMLMixin):
         self.cell(60, 8, f"Beneficiary ID: {beneficiary_id}")
         self.set_x(150)
         self.cell(60, 8, f"Ministry of Health Zambia")
-
 
     def print_medical_record_details(self, table_data):
         line_height = self.font_size * 2.5
@@ -141,13 +155,13 @@ class PDF(FPDF, HTMLMixin):
                         )
                     else:
                         self.multi_cell(
-                        col_width,
-                        line_height + 5,
-                        datum,
-                        border=1,
-                        ln=3,
-                        max_line_height=self.font_size,
-                    )
+                            col_width,
+                            line_height + 5,
+                            datum,
+                            border=1,
+                            ln=3,
+                            max_line_height=self.font_size,
+                        )
                 self.ln(line_height)
             else:
                 for index, datum in enumerate(row_data):
@@ -172,13 +186,13 @@ class PDF(FPDF, HTMLMixin):
                         )
                     else:
                         self.multi_cell(
-                        col_width,
-                        line_height,
-                        datum,
-                        border=1,
-                        ln=3,
-                        max_line_height=self.font_size,
-                    )
+                            col_width,
+                            line_height,
+                            datum,
+                            border=1,
+                            ln=3,
+                            max_line_height=self.font_size,
+                        )
                 self.ln(line_height)
 
     def print_records(self):
@@ -191,10 +205,11 @@ class PDF(FPDF, HTMLMixin):
             self.add_page()
 
             self.print_header(
-                35, f"Medical Record Entry as of: {record.created.strftime('%b %d %Y')}", 18
+                35,
+                f"Medical Record Entry as of: {record.created.strftime('%b %d %Y')}",
+                18,
             )
             self.line(10, 45, 200, 45)
-
 
             self.set_y(80)
             self.set_font("helvetica", "B", 11)
@@ -208,23 +223,37 @@ class PDF(FPDF, HTMLMixin):
             self.multi_cell(90, 8, f"Comments: {record.provider_comments}", ln=1)
 
             TABLE_HEADERS = [
-                    "Interraction Date",
-                    "Prescription",
-                    "No. of Days",
-                    "When to take ",
-                    "Lab comment",
-                ]
+                "Interraction Date",
+                "Prescription",
+                "No. of Days",
+                "When to take ",
+                "Lab comment",
+            ]
 
             table_data = [
                 TABLE_HEADERS,
             ]
-            table_data.append([record.interaction_date.strftime("%d %b %Y"), record.prescription, str(record.no_of_days), record.when_to_take, record.lab])
+            table_data.append(
+                [
+                    record.interaction_date.strftime("%d %b %Y"),
+                    record.prescription,
+                    str(record.no_of_days),
+                    record.when_to_take,
+                    record.lab,
+                ]
+            )
             self.print_medical_record_details(table_data)
 
 
 def create_document(filename, beneficiary_obj, medical_records):
     # Create a self object
-    pdf = PDF("P", "mm", "A4", beneficiary_obj=beneficiary_obj, medical_records=medical_records)
+    pdf = PDF(
+        "P",
+        "mm",
+        "A4",
+        beneficiary_obj=beneficiary_obj,
+        medical_records=medical_records,
+    )
 
     # get total page numbers
     pdf.alias_nb_pages()
@@ -235,4 +264,24 @@ def create_document(filename, beneficiary_obj, medical_records):
     pdf.print_beneficiary_details()
 
     pdf.print_records()
+
+    data = (
+        ("First name", "Last name", "Age", "City"),
+        ("Jules", "Smith", "34", "San Juan"),
+        ("Mary", "Ramos", "45", "Orlando"),
+        ("Carlson", "Banks", "19", "Los Angeles"),
+        ("Lucas", "Cimon", "31", "Saint-Mahturin-sur-Loire"),
+    )
+
+    pdf.add_page()
+    pdf.set_font("Times", size=10)
+    line_height = pdf.font_size * 2.5
+    col_width = pdf.epw / 4  # distribute content evenly
+    for row in data:
+        for datum in row:
+            pdf.multi_cell(
+                col_width, line_height, datum, ln=3, max_line_height=pdf.font_size
+            )
+        pdf.ln(line_height)
+    # pdf.output('table_with_cells.pdf')
     pdf.output(filename)
