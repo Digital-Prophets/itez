48ba7fdb2a736397b7786550041f9cc2bb008b4e
Jachin Manda <jachin.dev@outlook.com>
2021-12-28 16:02:40 +0200

    Remove link to UI elemments in sidebar and views.

--- itez/beneficiary/views.py
+++ itez/beneficiary/views.py
@@ -174,13 +174,6 @@ def poll_async_results(request, task_id):
         return JsonResponse({"error": f"No task with id {task_id}"}, status=400)
 
 
-@login_required(login_url="/login/")
-def uielements(request):
-    context = {"title": "UI Elements"}
-    html_template = loader.get_template("beneficiary/test_details.html")
-    return HttpResponse(html_template.render(context, request))
-
-
 class MedicalRecordListView(LoginRequiredMixin, ListView):
     template_name = "beneficiary/medical_record_list.html"
     model = MedicalRecord
