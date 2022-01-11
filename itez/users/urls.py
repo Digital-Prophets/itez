7487033d3cb005dbe1da6611c240e4aa96189e28
Digital-Prophets <olipa@digiprophets.com>
2022-01-11 08:40:23 +0200

    update user url for user create

--- itez/users/urls.py
+++ itez/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from itez.users.views import (
+    user_delete,
     user_detail_view,
     user_redirect_view,
     user_update_view,
@@ -11,7 +12,8 @@ from itez.users.views import (
 
 app_name = "users"
 urlpatterns = [
-    path("user/create", UserCreateView.as_view(), name="user_create"),
+    path("user/create/", UserCreateView.as_view(), name="user_create"),
+    path("user/delete/<int:user_id>", user_delete, name="delete"),
     path("user/profile/photo/upload/", user_profile_photo_update, name="profile_photo"),
     path("user/profile/", user_profile, name="profile"),
     path("~redirect/", view=user_redirect_view, name="redirect"),
