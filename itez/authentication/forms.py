887b8e197b8078b8f46ee60b9823c15dd678602c
sjtg <chipilej@gmail.com>
2022-03-15 10:59:13 +0200

    Modification the Itez Platform: 1: Added Graph , Bar Charts, Pie Charts 2: Medical Reports Form 3: Agrents Form 4: User Form

--- itez/authentication/forms.py
+++ itez/authentication/forms.py
@@ -1,60 +1,58 @@
 # -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2022 - Digital Prophets
-"""
 
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 
 class LoginForm(forms.Form):
     username = forms.CharField(
         widget=forms.TextInput(
-            attrs={
-                "placeholder": "Username",
-                "class": "form-control"
-            }
-        ))
+            attrs={"placeholder": "Username", "class": "form-control form-control-lg"}
+        )
+    )
     password = forms.CharField(
         widget=forms.PasswordInput(
-            attrs={
-                "placeholder": "Password",
-                "class": "form-control"
-            }
-        ))
+            attrs={"placeholder": "Password", "class": "form-control form-control-lg"}
+        )
+    )
 
 
-class SignUpForm(UserCreationForm):
+class UserCreationForm(forms.Form):
+    class Meta:
+        model = User
+
+        error_messages = {
+            "username": {"unique": _("This username has already been taken.")}
+        }
+
+
+class SignUpForm(forms.Form):
     username = forms.CharField(
         widget=forms.TextInput(
-            attrs={
-                "placeholder": "Username",
-                "class": "form-control"
-            }
-        ))
+            attrs={"placeholder": "Username", "class": "form-group col-md-6 mb-0"}
+        )
+    )
     email = forms.EmailField(
         widget=forms.EmailInput(
-            attrs={
-                "placeholder": "Email",
-                "class": "form-control"
-            }
-        ))
+            attrs={"placeholder": "Email", "class": "form-group col-md-6 mb-0"}
+        )
+    )
     password1 = forms.CharField(
         widget=forms.PasswordInput(
-            attrs={
-                "placeholder": "Password",
-                "class": "form-control"
-            }
-        ))
+            attrs={"placeholder": "Password", "class": "form-control form-control-lg"}
+        )
+    )
     password2 = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
                 "placeholder": "Password check",
-                "class": "form-control"
+                "class": "form-control form-control-lg",
             }
-        ))
+        )
+    )
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ("username", "email", "password1", "password2")
